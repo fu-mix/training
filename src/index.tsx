a8b0b861bf4b3e338e762f4efac622bb1398ca07
fumi <fuminaka@gmail.com>
2022-09-20 22:06:17 +0900

    Added: sample form

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import RegistrationForm from './form/RegistrationFrom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RegistrationForm />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
